9322d0ccaaaa8344b7d29b40ec16fd1b27404f78
Vito Van <awesomevito@live.com>
2019-12-19 17:53:58 +0800

    initial commit

---
+++ server.sh
@@ -0,0 +1,3 @@
+while { echo -en "HTTP/1.1 200 OK\r\nConnection: keep-alive\r\n\r\n$(date +'%Y-%m-%d %T') @ $(ifconfig eth0 | grep 'inet addr' | cut -d: -f2 | awk '{print $1}')\r\n"; } | nc -l -s 0.0.0.0 -p 8081; do
+  echo "================================================"
+done
