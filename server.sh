aa762545c76165851d1554650d9233f6160b27bc
Vito Van <awesomevito@live.com>
2019-12-19 18:34:51 +0800

    refactor: change default port 8080 -> 80

--- server.sh
+++ server.sh
@@ -1,3 +1,3 @@
-while { echo -en "HTTP/1.1 200 OK\r\nConnection: keep-alive\r\n\r\n$(date +'%Y-%m-%d %T') @ $(ifconfig eth0 | grep 'inet addr' | cut -d: -f2 | awk '{print $1}')\r\n"; } | nc -l -s 0.0.0.0 -p 8081; do
+while { echo -en "HTTP/1.1 200 OK\r\nConnection: keep-alive\r\n\r\n$(date +'%Y-%m-%d %T') @ $(ifconfig eth0 | grep 'inet addr' | cut -d: -f2 | awk '{print $1}')\r\n"; } | nc -l -s 0.0.0.0 -p 80; do
   echo "================================================"
 done
